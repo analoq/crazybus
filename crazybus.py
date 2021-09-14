257465ba8d564f3fac096c64254120f841ebce15
a. p. matthews <i@apmatthe.ws>
2021-09-13 19:20:25 -0700

    code styling

--- crazybus.py
+++ crazybus.py
@@ -1,3 +1,4 @@
+"""Generate Crazybus music and save to MIDI file"""
 from math import log2
 from random import randrange
 from mido import Message, MetaMessage, MidiFile, MidiTrack, bpm2tempo
@@ -5,24 +6,31 @@ from mido import Message, MetaMessage, MidiFile, MidiTrack, bpm2tempo
 MEASURES = 32
 TICKS_PER_BEAT = 120
 
-def pitch_to_hz(pitch : int) -> float:
+def pitch_to_hz(pitch: int) -> float:
+    """Convert 10-bit pitch to frequency in Hz"""
     return 3579545 / (32 * (1024 - pitch))
 
-def hz_to_note(frequency : float) -> float:
+def hz_to_note(frequency: float) -> float:
+    """Convert frequency in Hz to MIDI note number as float"""
     return 12*log2(frequency/440.0) + 69
 
-def random_pitch(offset : int) -> int:
+def random_pitch(offset: int) -> int:
+    """Generate a random 10-bit pitch given offset"""
     return (randrange(40)*10 + offset) % 0x3ff
 
-def bend_for_note(note : float) -> int:
+def bend_for_note(note: float) -> int:
+    """Determine the amount of pitch bend for a MIDI note number as float"""
     return round((note - int(note))*4096)
 
-if __name__ == "__main__":
-    assert(hz_to_note(880.0) == 69+12)
-    assert(hz_to_note(440.0) == 69+0)
-    assert(hz_to_note(220.0) == 69-12)
-    assert(bend_for_note(60.5) == 2048)
+def test():
+    """Test methods"""
+    assert hz_to_note(880.0) == 69+12
+    assert hz_to_note(440.0) == 69+0
+    assert hz_to_note(220.0) == 69-12
+    assert bend_for_note(60.5) == 2048
 
+def main():
+    """Main program"""
     mid = MidiFile(ticks_per_beat=TICKS_PER_BEAT)
     tempo_track = MidiTrack()
     tempo_track.append(MetaMessage('set_tempo', time=0, tempo=bpm2tempo(90)))
@@ -39,11 +47,26 @@ if __name__ == "__main__":
             bend = bend_for_note(note)
             note_floor = int(note) if note < 128 else 127
 
-            mid.tracks[track_num].append(
-                Message('pitchwheel', time=0, channel=track_num, pitch=bend))
-            mid.tracks[track_num].append(
-                Message('note_on', time=0, channel=track_num, note=note_floor, velocity=64))
-            mid.tracks[track_num].append(
-                Message('note_off', time=TICKS_PER_BEAT//4, channel=track_num, note=note_floor, velocity=127))
+            pitchwheel = Message('pitchwheel',
+                                 time=0,
+                                 channel=track_num,
+                                 pitch=bend)
+            note_on = Message('note_on',
+                              time=0,
+                              channel=track_num,
+                              note=note_floor,
+                              velocity=64)
+            note_off = Message('note_off',
+                               time=TICKS_PER_BEAT//4,
+                               channel=track_num,
+                               note=note_floor,
+                               velocity=127)
+            mid.tracks[track_num].append(pitchwheel)
+            mid.tracks[track_num].append(note_on)
+            mid.tracks[track_num].append(note_off)
 
     mid.save('output.mid')
+
+if __name__ == "__main__":
+    test()
+    main()
